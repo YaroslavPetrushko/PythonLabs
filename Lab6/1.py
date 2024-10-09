a03faebd20d4da9580a92c016ba20b5aef5e4177
YaroslavPetrushko <yaroslavpetrushko.off@gmail.com>
2024-10-09 15:15:16 +0300

    Update 1.py

--- Lab6/1.py
+++ Lab6/1.py
@@ -3,15 +3,14 @@ def insert_at_odd_positions():
     new_elements = list(map(int, input('Enter new elements to insert at odd positions (space-separated): ').split()))
     
     result = []
-    j = 0  # індекс для new_elements
+    j = 0
 
     for i in range(len(arr) + len(new_elements)):
-        # Вставляємо нові елементи на непарні позиції
+
         if (i % 2 == 0) and (j < len(new_elements)):
             result.append(new_elements[j])
-            j += 1  # переходимо до наступного елемента в new_elements
+            j += 1  
         else:
-            # Вставляємо елементи з arr на інші позиції
             result.append(arr[i - j])
 
     print(result)
