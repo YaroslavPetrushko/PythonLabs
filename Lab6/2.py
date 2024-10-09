14b90c7f939dca4e95a02597b94372da8133db6c
YaroslavPetrushko <yaroslavpetrushko.off@gmail.com>
2024-10-09 15:15:27 +0300

    Update 2.py

--- Lab6/2.py
+++ Lab6/2.py
@@ -1,11 +1,9 @@
 def replace_max_min():
     arr = list(map(int, input('Enter list elements (space-separated): ').split()))
     
-    # Знаходимо індекси мінімального та максимального елементів
     iMin = arr.index(min(arr))
     iMax = arr.index(max(arr))
 
-    # Міняємо місцями мінімальний і максимальний елемент
     arr[iMin], arr[iMax] = arr[iMax], arr[iMin]
 
     print(arr)
