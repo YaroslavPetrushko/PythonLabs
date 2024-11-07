31cd183f3b1b415061125ed805550701d94bc192
YaroslavPetrushko <yaroslavpetrushko.off@gmail.com>
2024-11-07 16:12:12 +0200

    Updating lab15

--- Lab15/Task2.py
+++ Lab15/Task2.py
@@ -13,3 +13,14 @@ fixed_df = pd.read_csv('data.csv',
 # Побудова графіка
 fixed_df.plot(figsize=(15, 10))
 plt.show()
+
+# Іизначаємо сумарну кількість велосипедистів за всіма місцями
+fixed_df['Total'] = fixed_df.sum(axis=1, numeric_only=True)
+
+# Групуємо дані за місяцями та рахуємо загальну кількість велосипедистів за місяць
+monthly_totals = fixed_df['Total'].resample('ME').sum()
+
+# Знаходимо місяць з найбільшою кількістю велосипедистів
+most_popular_month = monthly_totals.idxmax().strftime('%B')
+
+print("Місяць з найбільшою кількістю велосипедистів:", most_popular_month)
